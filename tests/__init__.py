38c5aff3cc67c5ccb5265ab6310c0c90a7db17e2
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-01-29 11:03:12 +0100

    Update testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+
+import unittest
+
+
+class TestStuff(unittest.TestCase):
+    def test_pass(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
