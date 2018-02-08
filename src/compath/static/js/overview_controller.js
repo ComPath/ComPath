c4c50877859fb491f09c1553e058383634904bc1
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-02-08 09:24:18 +0100

    Add new functionalities to vennDiagram

--- src/compath/static/js/overview_controller.js
+++ src/compath/static/js/overview_controller.js
@@ -87,14 +87,55 @@ $(document).ready(function () {
                 url: "/query/overlap?" + form.serialize(),
                 type: 'GET',
                 dataType: "json",
-                success: function (sets) {
-                    console.log(sets);
+                success: function (data) {
 
-                    var chart = venn.VennDiagram();
-                    d3.select("#overlap-venn-diagram").datum(sets).call(chart);
+                    // Adapted from https://github.com/benfred/venn.js/
+
+                    var Venndiv = d3.select("#overlap-venn-diagram");
+
+                    Venndiv.attr("align","center"); // Align center the diagram
+
+                    var geneOverlap = venn.VennDiagram(); // Plot the Venn Diagram
+                    Venndiv.datum(data).call(geneOverlap); // Stick data
+
+                    // add a tooltip
+                    var tooltip = d3.select("body").append("div")
+                        .attr("class", "venntooltip");
+
+                    // add listeners to all the groups to display tooltip on mouseover
+                    Venndiv.selectAll("g")
+                        .on("mouseover", function (d, i) {
+                            // sort all the areas relative to the current item
+                            venn.sortAreas(Venndiv, d);
+
+                            // Display a tooltip with the current size
+                            tooltip.transition().duration(400).style("opacity", .9);
+                            tooltip.text(d.size + " genes");
+
+                            // highlight the current path
+                            var selection = d3.select(this).transition("tooltip").duration(400);
+                            selection.select("path")
+                                .style("stroke-width", 3)
+                                .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
+                                .style("stroke-opacity", 1);
+                        })
+
+                        .on("mousemove", function () {
+                            tooltip.style("left", (d3.event.pageX) + "px")
+                                .style("top", (d3.event.pageY - 28) + "px");
+                        })
+
+                        .on("mouseout", function (d, i) {
+                            tooltip.transition().duration(400).style("opacity", 0);
+                            var selection = d3.select(this).transition("tooltip").duration(400);
+                            selection.select("path")
+                                .style("stroke-width", 0)
+                                .style("fill-opacity", d.sets.length == 1 ? .25 : .0)
+                                .style("stroke-opacity", 0);
+                        });
                 },
-                error: function (request) {
-                    alert(request.responseText);
+                error: function () {
+                    alert("Invalid Query. Please make sure you have selected at least 2 pathways.")
                 }
             });
 
