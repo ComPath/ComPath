ee8783ea8ab9df55b417f7eb2e15c909ccc0c26c
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-02-23 16:16:35 +0100

    Uncomment managers

--- src/compath/web.py
+++ src/compath/web.py
@@ -88,35 +88,35 @@ def create_app(connection=None):
 
     app.register_blueprint(ui_blueprint)
 
-    # app.manager_dict = {
-    #     name: ExternalManager(connection=connection)
-    #     for name, ExternalManager in managers.items()
-    # }
-    #
-    # log.info('Loading resource distributions')
-    #
-    # app.resource_distributions = {
-    #     name: manager.get_pathway_size_distribution()
-    #     for name, manager in app.manager_dict.items()
-    # }
-    #
-    # log.info('Loading overlap across pathway databases')
-    #
-    # resource_genesets = {
-    #     name: manager.get_all_hgnc_symbols()
-    #     for name, manager in app.manager_dict.items()
-    # }
+    app.manager_dict = {
+        name: ExternalManager(connection=connection)
+        for name, ExternalManager in managers.items()
+    }
+
+    log.info('Loading resource distributions')
+
+    app.resource_distributions = {
+        name: manager.get_pathway_size_distribution()
+        for name, manager in app.manager_dict.items()
+    }
+
+    log.info('Loading overlap across pathway databases')
+
+    resource_genesets = {
+        name: manager.get_all_hgnc_symbols()
+        for name, manager in app.manager_dict.items()
+    }
 
     # Get the universe of all HGNC symbols from Bio2BEL_hgnc and close the session
     log.info('Loading gene universe from bio2BEL_hgnc ')
-    #TODO Uncomment later
-    # hgnc_manager = HgncManager()
-    #
-    # resource_genesets['Gene Universe'] = hgnc_manager.get_all_hgnc_symbols()
-    #
-    # app.manager_overlap = process_overlap_for_venn_diagram(gene_sets=resource_genesets, skip_gene_set_info=True)
-    #
-    # hgnc_manager.session.close()
+    # TODO Uncomment later
+    hgnc_manager = HgncManager()
+
+    resource_genesets['Gene Universe'] = hgnc_manager.get_all_hgnc_symbols()
+
+    app.manager_overlap = process_overlap_for_venn_diagram(gene_sets=resource_genesets, skip_gene_set_info=True)
+
+    hgnc_manager.session.close()
 
     log.info('Done building %s in %.2f seconds', app, time.time() - t)
 
