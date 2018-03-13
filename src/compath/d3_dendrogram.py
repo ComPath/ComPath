d09a89436d69690a15738da077b45b8bed818586
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-03-13 18:10:28 +0100

    Update dendrogram waiting for stackoverflow answer

--- src/compath/d3_dendrogram.py
+++ src/compath/d3_dendrogram.py
@@ -108,4 +108,7 @@ def get_dendrogram_tree(gene_sets, pathway_manager_dict):
 
     label_tree(id_name_dict, pathway_manager_dict, cluster_to_x, d3_dendrogram["children"][0])
 
+    import json
+    print(json.dumps(d3_dendrogram))
+
     return d3_dendrogram
