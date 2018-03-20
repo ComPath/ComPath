2aa4cd7bbb4dbeb2866119eb2ffb4b546a62d1db
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-03-20 08:51:28 +0100

    Comment constants

--- src/compath/constants.py
+++ src/compath/constants.py
@@ -19,8 +19,11 @@ EXCEL_FOLDER = os.path.join(STATIC_FOLDER, 'resources', 'excel')
 REACTOME_GENE_SET = os.path.join(EXCEL_FOLDER, 'kegg_gene_sets.csv')
 KEGG_GENE_SET = os.path.join(EXCEL_FOLDER, 'reactome_gene_sets.csv')
 
+
+# Managers with hierarchical information
 HIERARCHY_MANAGERS = {'reactome'}
 
+# Managers without pathway knowledge
 BLACK_LIST = {
     'hgnc',
     'compath_hgnc',
