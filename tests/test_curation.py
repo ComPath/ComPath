76124ef675f474ab7474676b524fa7ff63241467
ddomingofernandez <danieldomingofernandez@hotmail.com>
2018-04-04 11:23:14 +0200

    Add more tests

--- tests/test_curation.py
+++ tests/test_curation.py
@@ -2,13 +2,14 @@
 
 """ This module contains tests for the parsing and processing of the curation exercises"""
 
-from compath.curation.curation import _syntax_checker
+from compath.constants import *
+from compath.curation.curation import _syntax_checker, _ensure_two_pathways, _get_pathways_from_statement
 from .constants import *
 
 
 class TestCurationParser(unittest.TestCase):
 
-    def test_syntax_checker(self):
+    def test_valid_syntax_examples(self):
         """Testing valid statements"""
         result_1 = _syntax_checker(VALID_MAPPING_1, PATHWAY_X)
         self.assertTrue(result_1)
@@ -19,7 +20,7 @@ class TestCurationParser(unittest.TestCase):
         result_3 = _syntax_checker(VALID_MAPPING_3, PATHWAY_X)
         self.assertTrue(result_3)
 
-    def test_syntax_checker_2(self):
+    def test_unvalid_syntax_examples(self):
         """Testing unvalid statements"""
         result_1 = _syntax_checker(INVALID_MAPPING_1, PATHWAY_X)
         self.assertFalse(result_1)
@@ -47,3 +48,25 @@ class TestCurationParser(unittest.TestCase):
 
         result_9 = _syntax_checker(INVALID_MAPPING_6, PATHWAY_X)
         self.assertFalse(result_9)
+
+    def test_two_pathways_in_statement(self):
+        """Testing two pathways statements"""
+        result_1 = _ensure_two_pathways(INVALID_MAPPING_5, IS_PART_OF)
+        self.assertFalse(result_1)
+
+        result_2 = _ensure_two_pathways(INVALID_MAPPING_6, IS_PART_OF)
+        self.assertFalse(result_2)
+
+        result_3 = _ensure_two_pathways(VALID_MAPPING_3, EQUIVALENT_TO)
+        self.assertTrue(result_3)
+
+    def test_get_pathways_from_statement(self):
+        """Testing getting the pathways fr om statements"""
+        subject, object = _get_pathways_from_statement(VALID_MAPPING_2, EQUIVALENT_TO)
+
+        self.assertEqual(subject, PATHWAY_X)
+        self.assertEqual(object, 'Pathway Y')
+
+        subject, object = _get_pathways_from_statement(VALID_MAPPING_3, EQUIVALENT_TO)
+        self.assertEqual(subject, PATHWAY_X)
+        self.assertEqual(object, PATHWAY_X)
