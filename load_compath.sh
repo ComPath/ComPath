2632e00ebcd01d922457b8db9c7af3ca802b81bc
Daniel Domingo-Fernández <daniel.domingo.fernandez@scai.fraunhofer.de>
2018-05-30 15:36:54 +0200

    Set specific version

--- load_compath.sh
+++ load_compath.sh
@@ -2,12 +2,10 @@
 
 # Create ComPath Container
 
-docker create -v /data --name compath-data docker.arty.scai.fraunhofer.de/compath:latest 
+docker create -v /data --name compath-data docker.arty.scai.fraunhofer.de/compath:0.0.1 
 
 # Create the Normal Execution Container
 
-# TODO: Copy from Juergen
-
-docker run --name=compath --volumes-from compath-data --restart=always -t -d -p 30050:5000 docker.arty.scai.fraunhofer.de/compath:latest
+docker run --name=compath --volumes-from compath-data --restart=always -t -d -p 30050:5000 docker.arty.scai.fraunhofer.de/compath:0.0.1
 
 docker exec -t -it compath /app/load_data.sh
