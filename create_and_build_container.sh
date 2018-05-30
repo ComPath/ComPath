3f7e01ac1aa708f35458153ad5aadf964993b3fb
Daniel Domingo-Fernández <daniel.domingo.fernandez@scai.fraunhofer.de>
2018-05-30 16:20:05 +0200

    Update create_and_build_container.sh

--- create_and_build_container.sh
+++ create_and_build_container.sh
@@ -9,3 +9,10 @@ docker create -v /data --name compath-data docker.arty.scai.fraunhofer.de/compat
 docker run --name=compath --volumes-from compath-data --restart=always -t -d -p 30050:5000 docker.arty.scai.fraunhofer.de/compath:0.0.1
 
 docker exec -t -it compath /app/load_data.sh
+
+
+# TODO: Create Email/Admin. 
+
+# Options for python email library: ksi.scai.fraunhofer.de as a SMTP host and SSMTP as protocol
+
+# TODO: Load mappings
