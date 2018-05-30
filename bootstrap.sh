0c5ba4a3968cae7288ab9052a6f241cecb9900a9
Daniel Domingo-Fernández <daniel.domingo.fernandez@scai.fraunhofer.de>
2018-05-30 16:14:29 +0200

    Add new file

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# I need a shut_down method, start method
+
+set -x
+
+pid=0
+
+shut_down() {
+    echo "shutting down"
+    
+    # Kills all python processes
+    ps aux |grep python |grep -v grep | awk '{print $2}' | while read line; do kill -HUP "$line"; done
+    sleep 10
+	ps aux
+    exit
+}
+
+trap 'shut_down' SIGKILL SIGTERM SIGHUP SIGINT EXIT
+
+python -m compath web --host 0.0.0.0 --port 5000 --template-folder="/opt/compath/src/compath/templates" --static-folder="/opt/compath/src/compath/static" >> /data/logs/compath.log 2>&1
+
+
+# this script must end with a persistent foreground process
+# exec a command
+# wait forever
+while true
+do
+	tail -f /data/logs/compath.log & wait ${!}
+done
